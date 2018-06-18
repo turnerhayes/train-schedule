1ce392896f4260276d811b0e15e9e1e6734c6a8c
Turner Hayes <dev@turnerhayes.com>
2018-06-18 01:29:17 -0400

    Fix hot reload path

--- app/app.js
+++ app/app.js
@@ -60,7 +60,7 @@ if (module.hot) {
   // Hot reloadable React components and translation json files
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept(['containers/App'], () => {
+  module.hot.accept(['components/App'], () => {
     ReactDOM.unmountComponentAtNode(MOUNT_NODE);
     render();
   });
